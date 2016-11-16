173845f0e6bea69ffb4c38b32ab8ed74d31a68a2
Joel Soderstrom <joel.soderstrom@gmail.com>
2016-11-16 13:42:29 +0100

    Adds fruster health

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const log = require('fruster-log');
-const fileService = require('./file-service');
-const conf = require('./conf');
+const log = require("fruster-log");
+const fileService = require("./file-service");
+const conf = require("./conf");
+
+require("fruster-health").start();
 
 fileService
 	.start(conf.port, conf.bus)
 	.then(function () {
-		log.info('File service started and connected to bus', conf.bus);
+		log.info("File service started and connected to bus", conf.bus);
 	})
 	.catch(function (err)  {
-		log.error('Failed starting file service', err);
+		log.error("Failed starting file service", err);
 		process.exit(1);
 	});
