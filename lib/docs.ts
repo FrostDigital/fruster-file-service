7fcfa5dbfaed018a9f04c63fb220635b3dc457ae
Joel Soderstrom <joel.soderstrom@gmail.com>
2021-01-07 23:42:24 +0100

    docs

--- lib/docs.ts
+++ lib/docs.ts
@@ -1,5 +1,4 @@
 import conf from "../conf";
-import constants from "./constants";
 
 const internalServerError = "Something unexpected happened.";
 const badRequest = "Request has invalid or missing fields.";
@@ -11,30 +10,27 @@ const docs = {
 		UPLOAD_FILE: {
 			description: `Uploads a file. Uses multipart data with the file to be uploaded as a form field called 'file'. Max file size is ${conf.maxFileSize} mb. Returns status code 201 on success.`,
 			errors: {
-				"file-service.400.1": "No file provided.",
-				"file-service.400.2": `File too large. Max size is ${conf.maxFileSize} mb`,
-				"file-service.403.1": "File type not allowed.",
-				"file-service.500.1": "Unknown error.",
-				[`${constants.serviceName}.INTERNAL_SERVER_ERROR`]: internalServerError,
-				[`${constants.serviceName}.BAD_REQUEST`]: badRequest
+				"FILE_NOT_PROVIDED": "No file provided.",
+				"FILE_TOO_LARGE": `File too large. Max size is ${conf.maxFileSize} mb`,
+				"FILE_TYPE_NOT_ALLOWED": "File type not allowed.",
+				INTERNAL_SERVER_ERROR: internalServerError,
+				BAD_REQUEST: badRequest
 			}
 		},
 
 		HEALTH: {
 			description: "Gets current health of the service.",
 			errors: {
-				"file-service.500.1": "Unknown error.",
-				[`${constants.serviceName}.INTERNAL_SERVER_ERROR`]: internalServerError,
-				[`${constants.serviceName}.BAD_REQUEST`]: badRequest
+				INTERNAL_SERVER_ERROR: internalServerError,
+				BAD_REQUEST: badRequest
 			}
 		},
 
 		UPDATE_IMAGE: {
 			description: "Update image - for rotate an image",
 			errors: {
-				"file-service.500.1": "Unknown error.",
-				[`${constants.serviceName}.INTERNAL_SERVER_ERROR`]: internalServerError,
-				[`${constants.serviceName}.BAD_REQUEST`]: badRequest
+				INTERNAL_SERVER_ERROR: internalServerError,
+				BAD_REQUEST: badRequest
 			}
 		}
 
@@ -45,8 +41,8 @@ const docs = {
 		DELETE_FILE: {
 			description: "Delete file from s3 bucket",
 			errors: {
-				[`${constants.serviceName}.INTERNAL_SERVER_ERROR`]: internalServerError,
-				[`${constants.serviceName}.BAD_REQUEST`]: badRequest
+				INTERNAL_SERVER_ERROR: internalServerError,
+				BAD_REQUEST: badRequest
 			}
 		}
 
